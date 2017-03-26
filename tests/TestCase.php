f9a01af686379df278c6f53eb53358114a6bd5f9
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-03-26 09:27:50 +0700

    Update Test Command

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,36 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use LaravelRocket\Foundation\Tests\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /** @var bool */
+    protected $useDatabase = false;
+
+    /** @var string  */
+    protected $baseUrl = 'http://localhost:8000';
+
+    /** @var \Faker\Generator */
+    protected $faker;
+
+    public function setUp()
+    {
+        parent::setUp();
+        if ($this->useDatabase) {
+            \DB::disableQueryLog();
+            exec('cp ' .database_path('testing/stubdb.sqlite') .' ' . database_path('testing/testdb.sqlite'));
+        }
+    }
+
+    public function tearDown()
+    {
+        if ($this->useDatabase) {
+            \DB::disconnect();
+            exec('rm ' . database_path('testing/testdb.sqlite'));
+        }
+        parent::tearDown();
+    }
 }

34b9010474734e6236dcb8c6cb4c9c0410bf6ca6
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-04-12 14:18:46 +0800

    Admin CSS Code Update

--- resources/views/pages/admin/auth/signin.blade.php
+++ resources/views/pages/admin/auth/signin.blade.php
@@ -18,14 +18,18 @@
 @stop
 
 @section('content')
-    <form action="{!! action('Admin\AuthController@postSignIn') !!}" method="post">
-        {!! csrf_field() !!}
-        <input type="email" name="email" placeholder="@lang('admin.pages.auth.messages.email')">
-        <input type="password" name="password" placeholder="@lang('admin.pages.auth.messages.password')">
-        <input type="checkbox" name="remember_me" value="1"> @lang('admin.pages.auth.messages.remember_me')
-        <button type="submit"
-                class="btn btn-primary btn-block btn-flat">@lang('admin.pages.auth.buttons.sign_in')</button>
-    </form>
-    <a href="{!! action('User\PasswordController@getForgotPassword') !!}">@lang('admin.pages.auth.messages.forgot_password')</a>
-    <br>
+<div class="c-auth-box__wrapper large-3 large-centered columns">
+    <div class="c-auth-box">
+        <form action="{!! action('Admin\AuthController@postSignIn') !!}" method="post">
+            {!! csrf_field() !!}
+            <input type="email" name="email" class="c-input-form" placeholder="@lang('admin.pages.auth.messages.email')">
+            <input type="password" name="password" class="c-input-form" placeholder="@lang('admin.pages.auth.messages.password')">
+            <input type="checkbox" name="remember_me" value="1"> @lang('admin.pages.auth.messages.remember_me')
+            <button type="submit"
+                    class="btn btn-primary btn-block btn-flat">@lang('admin.pages.auth.buttons.sign_in')</button>
+        </form>
+        <a href="{!! action('User\PasswordController@getForgotPassword') !!}">@lang('admin.pages.auth.messages.forgot_password')</a>
+        <br>
+    </div>
+</div>
 @stop

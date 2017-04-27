7396ccd1ed120453bbe173de5a69519b1c275ab0
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-04-27 18:45:45 +0700

    Update User Presenter

--- app/Presenters/UserPresenter.php
+++ app/Presenters/UserPresenter.php
@@ -1,8 +1,25 @@
 <?php
 namespace App\Presenters;
 
+use App\Models\File;
 use LaravelRocket\Foundation\Presenters\BasePresenter;
 
+/**
+ * @property \App\Models\User $entity
+ */
 class UserPresenter extends BasePresenter
 {
+    protected $multilingualFields = [];
+
+    protected $imageFields = ['profile_image'];
+
+    public function profileImage()
+    {
+        $image = $this->entity->profileImage;
+        if (!$image) {
+            $image = new File();
+        }
+
+        return $image;
+    }
 }

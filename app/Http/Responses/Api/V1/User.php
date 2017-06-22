9bdf8227df66e314629fad8f9d4b71b30ea3b3fc
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-06-22 16:20:33 +0800

    :+1: API Resoinse Added

---
+++ app/Http/Responses/Api/V1/User.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Responses\Api\V1;
+
+class User extends Response
+{
+    protected $columns = [
+        'id'           => '',
+        'name'         => '',
+        'profileImage' => null,
+    ];
+
+    /**
+     * @param \App\Models\User $model
+     *
+     * @return static
+     */
+    public static function updateWithModel($model)
+    {
+        $response = new static([], 400);
+        if (!empty($model)) {
+            $modelArray = [
+                'id'           => $model->id,
+                'name'         => $model->name,
+                'profileImage' => empty($model->profileImage) ? null : new Image($model->profileImage),
+            ];
+            $response = new static($modelArray, 200);
+        }
+
+        return $response;
+    }
+}

460de047a5e742fd50710153271eedb7c3a3b176
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-06-23 14:45:43 +0800

    :+1: Reference Issue

--- app/Http/Controllers/Api/V1/IndexController.php
+++ app/Http/Controllers/Api/V1/IndexController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\StatusRequest;
 use App\Http\Responses\Api\V1\Status;
 
 class IndexController extends Controller

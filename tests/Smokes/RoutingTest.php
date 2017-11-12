28d07f2d34f49e1a6d9db4bddaeac86ccdb0f1e0
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-11-12 18:10:33 +0800

    :bug: Routing Test Fix

--- tests/Smokes/RoutingTest.php
+++ tests/Smokes/RoutingTest.php
@@ -12,7 +12,7 @@ class RoutingTest extends TestCase
         $routeCollection = \Route::getRoutes();
 
         foreach ($routeCollection as $route) {
-            if (in_array('GET', $route->methods) && strpos($route->uri, '{') == -1) {
+            if (in_array('GET', $route->methods) && strpos($route->uri, '{') === FALSE ) {
                 $response = $this->call('GET', $route->uri);
                 $this->assertTrue(in_array($response->status(), [200, 201, 301, 302, 307]),
                     $route->uri.' produces error');

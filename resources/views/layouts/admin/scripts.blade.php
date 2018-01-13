b67d7853283f09a79320bc803aab1ce2fcc130f0
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-01-13 20:20:59 +0800

    :+1: Fontawesome update

--- resources/views/layouts/admin/scripts.blade.php
+++ resources/views/layouts/admin/scripts.blade.php
@@ -2,6 +2,7 @@
 <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
+<script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
 <script src="{!! \URLHelper::asset('libs/plugins/jQuery/jquery-2.2.3.min.js', 'admin') !!}"></script>
 <script src="{!! \URLHelper::asset('libs/bootstrap/js/bootstrap.min.js', 'admin') !!}"></script>
 <script src="{!! \URLHelper::asset('libs//plugins/iCheck/icheck.min.js', 'admin') !!}"></script>

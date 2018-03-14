5e99e4a26ca1ccf16fedf3e322ccb14982bf3047
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-03-14 20:25:21 +0800

    :+1: Admin CRUD Restructured

--- resources/assets/admin/src/components/Sidebar/_nav.js
+++ resources/assets/admin/src/components/Sidebar/_nav.js
@@ -3,28 +3,19 @@ export default {
     {
       name: 'Desktop',
       url: '/desktop',
-      icon: 'icon-speedometer'
+      icon: 'fa fa-desktop'
+    },
+    {
+      divider: true
     },
     {
       title: true,
-      name: 'UI elements',
-      wrapper: {            // optional wrapper object
-        element: "span",      // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ""             // optional class names space delimited list for title item ex: "text-center"
+      name: 'CRUDs'
     },
     {
-      name: 'Components',
+      name: 'Admin Users',
       url: '/admin-users',
-      icon: 'icon-puzzle',
-      children: [
-        {
-          name: 'Admin Users',
-          url: '/admin-users',
-          icon: 'icon-puzzle'
-        }
-      ]
-    }
+      icon: 'fa fa-user-secret'
+    },
   ]
 };

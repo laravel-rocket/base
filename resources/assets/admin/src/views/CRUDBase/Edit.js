ec3585f9d43125ad5329b3b3c01dd04f5390c94f
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-10-31 08:59:32 +0900

    :bug: Admin CRUD fix

--- resources/assets/admin/src/views/CRUDBase/Edit.js
+++ resources/assets/admin/src/views/CRUDBase/Edit.js
@@ -25,14 +25,15 @@ class Edit extends Base {
     this.get = this.get.bind(this);
     this.handleOnSubmit = this.handleOnSubmit.bind(this);
     this.getRelationCandidates = this.getRelationCandidates.bind(this);
+    this.handleModelChange = this.handleModelChange.bind(this);
   }
 
   setInitialState(props) {
     this.state = {
       model: {},
+      initialModel: {},
       id: 0,
-      relationModels: {},
-      errors: {}
+      relationModels: {}
     };
   }
 
@@ -50,49 +51,29 @@ class Edit extends Base {
   }
 
   handleOnSubmit(formData) {
-
     const id = this.state.id;
     if (id > 0) {
       this.repository.update(id, formData).then(repos => {
-        if (repos.errorCode) {
-          let errors = [];
-          for (let i = 0; i < repos.invalidParams.length; i++) {
-            errors[repos.invalidParams[i].name] = repos.invalidParams[i].message
-          }
-          this.setState({
-            ...this.state,
-            errors: errors
-          });
-          this.props.methods.errorMessage('Failed ' + repos.detail);
-        } else {
-          this.props.history.push(this.path + '/' + id);
-          this.props.methods.successMessage('Successfully Updated ');
-        }
+        this.props.history.push(this.path + '/' + id);
+        this.props.methods.successMessage('Successfully Updated ');
       });
     } else {
       this.repository.store(formData).then(repos => {
-        if (repos.errorCode) {
-          let errors = [];
-          for (let i = 0; i < repos.invalidParams.length; i++) {
-            errors[repos.invalidParams[i].name] = repos.invalidParams[i].message
-          }
-          this.setState({
-            ...this.state,
-            errors: errors
-          });
-          this.props.methods.errorMessage('Failed ' + repos.detail);
-        } else {
-          this.props.methods.successMessage('Successfully Created ');
-          this.props.history.push(this.path + '/' + repos.id);
-        }
+        this.props.history.push(this.path + '/' + repos.id);
       });
     }
-    return false;
+  }
+
+  handleModelChange(model) {
+    this.setState({
+      model: model,
+    });
   }
 
   get(id) {
     this.repository.show(id).then(repos => {
-      this.setState({id: id, model: repos});
+      this.setState({id: id, model: repos, initialModel: repos});
+      console.log(this.state);
     }).catch(error => {
       this.props.methods.errorMessage('Data Fetch Failed. Please access again later');
     });
@@ -100,11 +81,11 @@ class Edit extends Base {
 
   getRelationCandidates(name, searchWord) {
     if( !this.relationRepositories[name] ) {
-      return Promise.resolve({ options: [] });
-    }
-
-    if (!searchWord) {
-      return Promise.resolve({ options: [] });
+      console.log('No Repositories');
+      console.log(this.relationRepositories);
+      if (!searchWord) {
+        return Promise.resolve({ options: [] });
+      }
     }
 
     const repository = this.relationRepositories[name];
@@ -113,10 +94,14 @@ class Edit extends Base {
       const relations = {...this.state.relationModels};
       relations[name] = repos.items;
       this.setState({
+        ...this.state,
         'relationModels': relations
       });
+      console.log(this.state);
+      console.log(relations);
       return {options: repos.items};
     }).catch(error => {
+      console.log(error);
       this.props.methods.errorMessage('Data Fetch Failed. Please access again later');
     });
   }
@@ -135,10 +120,11 @@ class Edit extends Base {
                   columns={this.columns.edit}
                   columnInfo={this.columns.columns}
                   model={this.state.model}
+                  initialModel={this.state.initialModel}
                   selectCandidates={this.relationModels}
                   onSubmit={this.handleOnSubmit}
                   onSelect={this.getRelationCandidates}
-                  errors={this.state.errors}
+                  onModelChange={this.handleModelChange}
                 />
               </CardBlock>
             </Card>
